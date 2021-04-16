406d180e8a0a0599eff47c23346caa1572fd5ea0
Nam-fullstack <namnguyen13_@hotmail.com>
2021-04-16 22:22:17 +1000

    Added in id navigation jump from an anchor from one page to another.

--- navbar.js
+++ navbar.js
@@ -1,15 +1,13 @@
 // Get position of scroll bar and determines if user has scrolled down. When user scrolls down
 // it hides the navbar. When the user scrolls up, the navbar appears.
 
-var prevScrollPosition = window.pageYOffset
-window.onscroll = function() {
-  var currentScrollPosition = window.pageYOffset
+let prevScrollPosition = window.pageYOffset
+window.onscroll = function () {
+  const currentScrollPosition = window.pageYOffset
   if (prevScrollPosition > currentScrollPosition) {
-    document.getElementById("navigation-bar").style.top = "0";
-  }
-
-  else {
-    document.getElementById("navigation-bar").style.top = "-70px";
+    document.getElementById('navigation-bar').style.top = '0'
+  } else {
+    document.getElementById('navigation-bar').style.top = '-70px'
   }
   prevScrollPosition = currentScrollPosition
 }
