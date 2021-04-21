c8d1ead1e04d5df2b16aa685744cb33a7936c2a6
Nam-fullstack <namnguyen13_@hotmail.com>
2021-04-21 15:33:33 +1000

    Added back missing code due to vim and error I had when pulling from git to main. Not sure if any other code or comments are missing. But need to move on with getting everything done.

---
+++ formspree.js
@@ -0,0 +1,30 @@
+<script>
+formbutton("create", {
+    action: "https://formspree.io/f/xvodgwvp",
+    title: "How can we help?",
+    fields: [
+      { 
+        type: "email", 
+        label: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "gray"
+      },
+      button: {
+        backgroundColor: "gray"
+      }
+    }
+  });
+</script>
